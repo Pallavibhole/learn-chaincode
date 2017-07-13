c09e9e05b1d5b78b6032bde4b14a6d38c69f987c
Pallavibhole <vibhooti94@gmail.com>
2017-07-13 15:13:10 +0530

    Update emp.go

--- emp.go
+++ emp.go
@@ -27,9 +27,9 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
-	else{
+	/*else{
 		fmt.Printf("Chaincode Started Successfully")
-	}
+	}*/
 }
 
 // Init resets all the things
