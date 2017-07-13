7e39d870f0fbe8999e5e9ddbbfdfb96455b80c68
Pallavibhole <vibhooti94@gmail.com>
2017-07-13 14:48:33 +0530

    Add files via upload

---
+++ emp.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+//Product - Structure for products used in buy goods
+type Employee struct {
+	EmpName string  `json:"empname"`
+	EmpId int64 `json:"empid"`
+	Place string  `json:"place"`
+	PhoneNo int64 `json:"phoneno"`
+}
+
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
+}
+
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+	else{
+		fmt.Printf("Chaincode Started Successfully")
+	}
+}
+
+// Init resets all the things
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+
+	err := stub.PutState("hello_world", []byte(args[0]))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// Invoke isur entry point to invoke a chaincode function
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("invoke is running " + function)
+
+	// Handle different functions
+	if function == "init" {
+		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		return t.write(stub, args)
+	} else if function == "adddetails" {
+		return t.adddetails(stub, args)
+	}
+	fmt.Println("invoke did not find func: " + function)
+
+	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+// Query is our entry point for queries
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+
+	// Handle different functions
+	if function == "read" { //read a variable
+		return t.read(stub, args)
+	} else if function == "readdetails" {
+		return t.readdetails(stub, args)
+	}
+	fmt.Println("query did not find func: " + function)
+
+	return nil, errors.New("Received unknown function query: " + function)
+}
+
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
+func (t *SimpleChaincode) adddetails(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("adding employee information")
+	if len(args) != 4 {
+		return nil, errors.New("Incorrect Number of arguments.Expecting 4 for adddetails")
+	}
+	amt, err := strconv.ParseFloat(args[1], 64)
+	
+
+	employee := Employee{
+		EmpName:   args[0],
+		EmpId: args[1],
+		Place: args[2],
+		Phoneno: args[3],
+	}
+
+	bytes, err := json.Marshal(employee)
+	if err != nil {
+		fmt.Println("Error marshaling employee")
+		return nil, errors.New("Error marshaling employee")
+	}
+
+	err = stub.PutState(employee.EmpId, bytes)
+	if err != nil {
+		return nil, err
+}
+return nil, nil
+}
+
+func (t *SimpleChaincode) readdetails(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("read() is running")
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. expecting 1")
+	}
+
+	key := args[0] // name of Entity
+	fmt.Println("key is ")
+	fmt.Println(key)
+	bytes, err := stub.GetState(args[0])
+	fmt.Println(bytes)
+	if err != nil {
+		fmt.Println("Error retrieving " + key)
+		return nil, errors.New("Error retrieving " + key)
+	}
+	/*
+	product := Product{}
+	err = json.Unmarshal(bytes, &product)
+	if err != nil {
+		fmt.Println("Error Unmarshaling customerBytes")
+		return nil, errors.New("Error Unmarshaling customerBytes")
+	}
+	
+	bytes, err = json.Marshal(product)
+	if err != nil {
+		fmt.Println("Error marshaling customer")
+		return nil, errors.New("Error marshaling customer")
+	}
+	fmt.Println(bytes)
+	*/
+	return bytes, nil
+}
